91e0e37a42b1a83d3177358e6d05aaca86e1c75f
Fjalar <fjalar@fjalar.org>
2023-09-14 16:09:44 +1000

    Work on conductor module.

--- src/conductor.jl
+++ src/conductor.jl
@@ -3,15 +3,54 @@ using Distributions, StatsBase
 
 # Number of clients.
 nclients = 2
+# Service request intensity, in mean requests per day.
+λ = 1 / Dates.days(Year(1))
 
-# Define the time frame.
-epoch = Date(2020)
-eschaton = Date(2023)
-era = eschaton - epoch
-today = epoch
+struct Metronome
+    epoch::Date
+    eschaton::Date
+end
+
+Metronome() = Metronome(Date(2020), Date(2023))
+
+function days(epoch::Date, eschaton::Date)
+    return epoch:Day(1):eschaton
+end
+
+function days(m::Metronome)
+    return days(m.epoch, m.eschaton)
+end
+
+function events(from_date, to_date, lambda)
+    # Get a list of the dates under consideration.
+    dates = days(from_date, to_date)
+    # Get the overall intensity for the period.
+    Λ = length(dates)*lambda
+    # Get the number of events from the corresponding Poisson distribution.
+    nevents = rand(Poisson(Λ))
+    # Sample the events uniformly and without replacement from the dates.
+    # TODO: This can return an empty list.
+    events = sample(dates, nevents; replace=false)
+    # Deliver as a sorted list.
+    return sort(events)
+end
+
+struct Client
+    dayzero::Date     # Date of entering the scheme, e.g. date of accident.
+    severity::Float64 # Multiplies the intensity of the request point process.
+end
+
+Client() = Client(rand(days(Metronome())), 1+rand(Exponential()))
+
+function client_events(client::Client, to_date::Date)
+    return events(client.dayzero, to_date, client.severity/Dates.days(Year(1)))
+end
+
+
 
 # Define a 'history' object tracking states of each agent over the time steps.
-history = Dict(i => [ state() ] for i in 1:nclients)
+history = Dict(i => Dict( date => [ state() ] for i in 1:nclients
+                                              for date in dates ) )
 
 
 # Make a naive bimodal distribution using Gaussians.
