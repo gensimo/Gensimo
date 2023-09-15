5be5608a397f82b8de59f00e0fa57396036b9edc
Fjalar <fjalar@fjalar.org>
2023-09-15 15:54:03 +1000

    Minor changes.

--- src/conductor.jl
+++ src/conductor.jl
@@ -65,14 +65,7 @@ ds = epoch:Day(1):eschaton # From beginning to end, one day at a time.
 ps = f.(Dates.value.(ds)) / sum(f.(Dates.value.(ds)))
 pmf = DiscreteNonParametric(Dates.value.(ds), ps)
 
-# From: https://en.wikipedia.org/wiki/Birth_process
-function p( n # Size of initial population.
-          , k # Size of projected population.
-          , t # Time.
-          , λ # Rate.
-          )
-    m = k - n
-    return binomial(n, m) * (λ*t)^m * (1 - λ*t)^(n-m)
-end
+# TODO: structure and constructor to set up a simulation object, i.e. Conductor.
+
 
 
