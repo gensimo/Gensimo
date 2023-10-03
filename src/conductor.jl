de495348796220ac59c199911b0203dce9f2c48c
Fjalar <fjalar@fjalar.org>
2023-10-03 17:13:01 +1100

    Added client-history plotting functionality.

--- src/conductor.jl
+++ src/conductor.jl
@@ -79,7 +79,7 @@ function simulate!(conductor::Conductor)
         # Add a client agent for this case.
         add_agent!(Client, model, case.state)
         # Run model for as many steps as events in case. Collect data.
-        dates = collect(keys(conductor.histories[case])) # Dates of events.
+        dates = sort(collect(keys(conductor.histories[case]))) # Event dates.
         n = length(dates) - 1 # Number of events, minus one for initial state.
         df, _ = run!(model, agent_step!, model_step!, n, adata=[:state]) # Data.
         # Keep only `Client` agent `State`s. Convert from `State?` type.
@@ -93,13 +93,43 @@ end
 function extract( conductor::Conductor
                 ; what=:costs )
     return Dict( case =>
-                 ( [ collect(keys(c.histories[case])) ]
+                 ( collect(keys(c.histories[case]))
                  # TODO: Convert below to Float64.
-                 , [ cost.(collect(values(c.histories[case]))) ] )
+                 , cost.(collect(values(c.histories[case]))) )
                  for case in conductor.cases )
 end
 
-
+# TODO: Migrate to display.jl.
+function datesplot(dates::Vector{Date}, values, labels=nothing)
+    # Use a decent Garamond for the plot.
+    set_theme!( fontsize=14
+              , fonts=(
+                      ; regular="Garamond"
+                      , bold="Garamond Bold"
+                      , italic="Garamond Italic"
+                      , bold_italic="Garamond Bold Italic" )
+              )
+    # Obtain fig and ax objects.
+    fig = Figure()
+    ax = Axis(fig[1, 1])
+    # Convert dates to integers, i.e. days since rounding epoch.
+    days = Dates.date2epochdays.(dates)
+    # Plot against those integers. Put labels if provided.
+    plt = scatterlines!(ax, days, values)
+    if !isnothing(labels)
+        text!( labels
+            , position=collect(zip(days, values))
+            , align=(:left, :bottom) )
+    end
+    # Then put the dates in place of those integers.
+    ax.xticks = (days, string.(dates))
+    # Quarter π rotation to avoid clutter.
+    ax.xticklabelrotation = π/4
+    # Show me what you got.
+    display(fig)
+    # Deliver.
+    return fig, ax, plt
+end
 
 
 
